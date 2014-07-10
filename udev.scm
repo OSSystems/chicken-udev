677cb07e0b87a94760618661d86a0d781d2cb6c1
Mario Domenech Goulart <mario@ossystems.com.br>
2014-07-10 14:08:56 -0300

    Wrapper for make-udev-device which accepts a single argument
    Signed-off-by: Mario Domenech Goulart <mario@ossystems.com.br>

--- udev.scm
+++ udev.scm
@@ -58,6 +58,23 @@
 
 (define-record udev-device node subsystem type action syspath sysname sysnum)
 
+(define %make-udev-device make-udev-device)
+
+(define-syntax udev-device-get-string
+  (syntax-rules ()
+    ((_ c-func)
+     (foreign-lambda c-string c-func (c-pointer (struct "udev_device"))))))
+
+(define (make-udev-device dev)
+  (%make-udev-device
+   ((udev-device-get-string "udev_device_get_devnode") dev)
+   ((udev-device-get-string "udev_device_get_subsystem") dev)
+   ((udev-device-get-string "udev_device_get_devtype") dev)
+   ((udev-device-get-string "udev_device_get_action") dev)
+   ((udev-device-get-string "udev_device_get_syspath") dev)
+   ((udev-device-get-string "udev_device_get_sysname") dev)
+   ((udev-device-get-string "udev_device_get_sysnum") dev)))
+
 (define-record-printer (udev-device obj out)
   (fprintf out
            "#<device node: ~S subsystem: ~S type: ~S action: ~S syspath: ~S sysname: ~S sysnum: ~S>"
@@ -69,23 +86,10 @@
            (udev-device-sysname obj)
            (udev-device-sysnum obj)))
 
-(define-syntax udev-device-get-string
-  (syntax-rules ()
-    ((_ c-func)
-     (foreign-lambda c-string c-func (c-pointer (struct "udev_device"))))))
-
 (define (receive-device mon)
   (let ((dev (%receive-device mon)))
     (and dev
-         (let ((udev-dev
-                (make-udev-device
-                 ((udev-device-get-string "udev_device_get_devnode") dev)
-                 ((udev-device-get-string "udev_device_get_subsystem") dev)
-                 ((udev-device-get-string "udev_device_get_devtype") dev)
-                 ((udev-device-get-string "udev_device_get_action") dev)
-                 ((udev-device-get-string "udev_device_get_syspath") dev)
-                 ((udev-device-get-string "udev_device_get_sysname") dev)
-                 ((udev-device-get-string "udev_device_get_sysnum") dev))))
+         (let ((udev-dev (make-udev-device dev)))
            (unref-device dev)
            udev-dev))))
 
